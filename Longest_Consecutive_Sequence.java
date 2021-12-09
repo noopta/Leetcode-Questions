a64f90ee309c74ba62f5e87e1bba15f1727f34b9
Anupta Islam <anuptaislam33@gmail.com>
2021-12-09 00:17:12 -0500

    Adding the optimal solution for the leetcode question longest consecutive sequence

--- Longest_Consecutive_Sequence.java
+++ Longest_Consecutive_Sequence.java
@@ -1,87 +1,32 @@
 class Solution {
-
-    public void mergeSort(int[] nums) {
-        int arrayLen = nums.length;
-
-        if (arrayLen < 2) {
-            return;
-        }
-
-        int midPoint = arrayLen / 2;
-        int[] leftArray = new int[midPoint];
-        int[] rightArray = new int[arrayLen - midPoint];
-        int i = 0;
-        int j = 0;
-
-        for (i = 0; i < midPoint; i++) {
-            leftArray[i] = nums[i];
-        }
-
-        for (i = midPoint; i < arrayLen; i++) {
-            rightArray[j] = nums[i];
-            j++;
-        }
-
-        mergeSort(leftArray);
-        mergeSort(rightArray);
-        merge(leftArray, rightArray, nums);
-    }
-
-    public void merge(int[] leftArray, int[] rightArray, int[] originalArray) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        while (i < leftArray.length && j < rightArray.length) {
-            if (leftArray[i] <= rightArray[j]) {
-                originalArray[k] = leftArray[i];
-                i++;
-            } else if (rightArray[j] < leftArray[i]) {
-                originalArray[k] = rightArray[j];
-                j++;
-            }
-
-            k++;
-        }
-
-        while (i < leftArray.length) {
-            originalArray[k] = leftArray[i];
-            i++;
-            k++;
-        }
-
-        while (j < rightArray.length) {
-            originalArray[k] = rightArray[j];
-            j++;
-            k++;
-        }
-    }
-
     public int longestConsecutive(int[] nums) {
-        // this can be good practice for a sorting algorithm
-        // learn merge sort
-        int i = 0;
-        int maxCount = 1;
-        int localCount = 1;
+        int globalSequence = 0;
+        HashMap<Integer, Integer> theMap = new HashMap<Integer, Integer>();
+        int currentSequence = 0;
 
-        if (nums.length == 0) {
-            return 0;
-        }
-
-        mergeSort(nums);
-
-        for (i = 1; i < nums.length; i++) {
-            if (nums[i] == (nums[i - 1] + 1)) {
-                System.out.println(localCount);
-                localCount++;
-                if (localCount > maxCount) {
-                    maxCount = localCount;
-                }
-            } else if (!(nums[i] == nums[i - 1])) {
-                localCount = 1;
+        for (int num : nums) {
+            if (theMap.containsKey(num)) {
+                theMap.put(num, theMap.get(num) + 1);
+            } else {
+                theMap.put(num, 1);
             }
         }
 
-        return maxCount;
+        for (int num : theMap.keySet()) {
+            if (!theMap.containsKey(num - 1)) {
+                int currentNum = num;
+                currentSequence = 1;
+
+                while (theMap.containsKey(currentNum + 1)) {
+                    currentSequence++;
+                    currentNum++;
+                }
+
+                globalSequence = Math.max(globalSequence, currentSequence);
+
+            }
+        }
+
+        return globalSequence;
     }
 }
