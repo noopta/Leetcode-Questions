60a8cc364b2ad6056b7afbce8fa86f2397590044
Anupta Islam <anuptaislam33@gmail.com>
2022-04-02 04:10:15 -0400

    Time: 1483 ms (5.03%), Space: 103.9 MB (5.05%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while(left < right){
+            System.out.print("LEFT = " + s.charAt(left) + " RIGHT = " + s.charAt(right));
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        
+        System.out.println();
+        
+        return true;
+    }
+    
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        int charsDeleted = 0;
+        StringBuilder tempString = new StringBuilder(s);
+        
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                if(isPalindrome(tempString.substring(left + 1, right + 1)) || isPalindrome(tempString.substring(left, right))){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            
+            left++;
+            right--;
+        }
+        
+        
+        return true;
+    }
+}
