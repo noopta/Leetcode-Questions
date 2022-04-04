716072ceafb8bf6e50082470e8364574b3072938
Anupta Islam <anuptaislam33@gmail.com>
2022-04-04 04:03:26 -0400

    Time: 12 ms (12.21%), Space: 59.6 MB (80.14%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode listPointer = head;
+        ListNode firstPointer = head;
+        ListNode secondPointer = head;
+        int kThFromStart = 0 + k;
+        int kThFromEnd = 0;
+        int listLen = 0;
+        int startVal = 0;
+        int endVal = 0;
+        int firstCounter = 0;
+        int secondCounter = 0;
+        
+        while(listPointer != null){
+            listLen++;    
+            listPointer = listPointer.next;
+        }
+        
+        kThFromEnd = (listLen) - k;
+        
+        System.out.println(kThFromStart + " " + kThFromEnd);
+        
+        while(firstPointer != null && firstCounter != kThFromStart - 1){
+            firstPointer = firstPointer.next;
+            firstCounter++;
+        }
+        
+        
+        startVal = firstPointer.val;
+        
+        while(secondPointer != null && secondCounter != kThFromEnd){
+            secondPointer = secondPointer.next;
+            secondCounter++;
+        }
+        
+        endVal = secondPointer.val;
+        
+        firstPointer.val = endVal;
+        secondPointer.val = startVal;
+        
+        System.out.println(startVal + " " + endVal);
+        
+        
+        
+        return head;
+    }
+}
