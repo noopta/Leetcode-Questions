792e0a641f3b4757910ddbfee30114ae3a52b92a
Anupta Islam <anuptaislam33@gmail.com>
2022-04-29 22:58:05 -0400

    Time: 10 ms (49.70%), Space: 77.3 MB (30.95%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -14,17 +14,18 @@
  * }
  */
 class Solution {
-    Integer ans = 0;
+    int maxCount = 0;
+    
     public int longestZigZag(TreeNode root) {
-        helper(root, true, 0);
-        return ans;
+        depthFirstSearch(root, true, 0);    
+        return maxCount;
     }
     
-    public void helper(TreeNode root, boolean isLeftChild, int count){
-        if (root != null) {
-            ans = Math.max(ans, count);
-            helper(root.right, false, isLeftChild ? count+1: 1);
-            helper(root.left, true, isLeftChild ? 1 : count+1);
+    public void depthFirstSearch(TreeNode root, boolean isLeftChild, int count){
+        if(root != null){
+            maxCount = Math.max(maxCount, count);
+            depthFirstSearch(root.right, false, isLeftChild ? count + 1 : 1);
+            depthFirstSearch(root.left, true, isLeftChild ? 1 : count + 1);
         }
     }
 }
