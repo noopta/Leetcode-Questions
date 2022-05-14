af6668187179b7b26e98c488ef9e5eb9763f590c
Anupta Islam <anuptaislam33@gmail.com>
2022-05-14 08:27:13 -0400

    Time: 13 ms (74.38%), Space: 44.5 MB (97.39%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,32 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] kFrequent = new int[k];
-        HashMap<Integer, Integer> theMap = new HashMap<Integer, Integer>();
-        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>((a, b) -> theMap.get(a) - theMap.get(b));
+        int[] kMostFrequent = new int[k];
+        HashMap<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>((a, b) -> frequencyMap.get(a) - frequencyMap.get(b));
         int i = 0;
         
-        for(i = 0; i < nums.length; i++){
-            if(theMap.containsKey(nums[i])){
-                theMap.put(nums[i], theMap.get(nums[i]) + 1);
+        for(int currentNum : nums){
+            if(frequencyMap.containsKey(currentNum)){
+                frequencyMap.put(currentNum, frequencyMap.get(currentNum) + 1);
             } else {
-                theMap.put(nums[i], 1);
+                frequencyMap.put(currentNum, 1);
             }
         }
         
-        for(int currentKey : theMap.keySet()){
-           minHeap.add(currentKey);
+        for(int currentKey : frequencyMap.keySet()){
+            minHeap.add(currentKey);
             
             if(minHeap.size() > k){
                 minHeap.poll();
             }
         }
         
-        i = 0;
-        
         while(!minHeap.isEmpty()){
-            kFrequent[i++] = minHeap.poll();    
+            kMostFrequent[i++] = minHeap.poll();
         }
         
-        return kFrequent;
+        return kMostFrequent;
     }
 }
