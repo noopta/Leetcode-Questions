32258ae15233ea97099bc4735e41d1dea48194b5
Anupta Islam <anuptaislam33@gmail.com>
2022-05-26 20:32:38 -0400

    Time: 909 ms (5.04%), Space: 67.9 MB (5.85%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int maxLen = 0;
+        HashMap<Character, Integer> theMap = new HashMap<Character, Integer>();
+        int left = 0;
+        int right = 0;
+        int currentMax = 0;
+        
+        while(right < s.length()){
+            
+            if(theMap.containsKey(s.charAt(right))){
+                theMap.put(s.charAt(right), theMap.get(s.charAt(right)) + 1);
+                
+            } else {
+                theMap.put(s.charAt(right), 1);
+            }
+            
+            for(char currentKey : theMap.keySet()){
+              currentMax = Math.max(currentMax, theMap.get(currentKey));
+            }
+            
+            System.out.println(s.charAt(right) + " current max = " + currentMax);
+            if ((right - left + 1)  - currentMax > k){
+                System.out.println(s.charAt(right));
+                
+                while((right - left + 1) - currentMax > k){
+                    
+                    for(char currentKey : theMap.keySet()){
+                      currentMax = Math.max(currentMax, theMap.get(currentKey));
+                    }
+                    theMap.put(s.charAt(left), theMap.get(s.charAt(left)) - 1);
+
+                    if(theMap.get(s.charAt(left)) <= 0){
+                        theMap.remove(s.charAt(left));
+                    }
+
+                    left++;
+                }
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+            
+            
+            right++;
+        }
+        
+        return maxLen;
+    }
+}
