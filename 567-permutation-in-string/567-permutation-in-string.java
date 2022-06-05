d2ba66328439ca7031ed74a6cce77ed20e41d1da
Anupta Islam <anuptaislam33@gmail.com>
2022-06-05 18:55:09 -0400

    Time: 8 ms (65.94%), Space: 41.8 MB (98.06%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,39 +1,19 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        // first check an edge case where s1.length() > s2.length()
-        // if true then return false
-        // else continue with the algorithm
-        // create a map for s1, and a map for s2, these can be 2 arrays 
-        // initialize the array with the first s1.length() characters and their frequencies 
-        // have one loop to first calculate the number of matching characters 
-        // then run a loop for the sliding window algorithm until right == s2.length()
-        // calculate the index for the char of s2.charAt(right)
-        // increment the count of this char in s2Map
-        // if s2Map[index] == s1Map[index], then increment numMatches
-        // if s2Map[index] - 1 == s1[Map], then decrement numMatches
-        // then calculate the index for s2.charAt(left)
-        // decrement s2Map for the respective character at the left index
-        // if s1Map[left] == s2Map[left], then increment numMatches
-        // if s1Map[left] - 1 == s2Map[left], then decrement numMatches
-        // then increment left, and right pointers
         if(s1.length() > s2.length()){
             return false;
         }
         
-        int numMatches = 0;
-        int s1Map[] = new int[26];
-        int s2Map[] = new int[26];
+        int []s1Map = new int[26];
+        int []s2Map = new int[26];
+        int i = 0;
         int left = 0;
         int right = s1.length();
-        int i = 0;
-        int currentIndex = 0;
+        int numMatches = 0;
         
         for(i = 0; i < s1.length(); i++){
-            currentIndex = (int)(s1.charAt(i)) - 97;
-            s1Map[currentIndex]++;
-            
-            currentIndex = (int)(s2.charAt(i)) - 97;
-            s2Map[currentIndex]++;
+            s1Map[(int)(s1.charAt(i)) - 97]++;
+            s2Map[(int)(s2.charAt(i)) - 97]++;
         }
         
         for(i = 0; i < 26; i++){
@@ -42,24 +22,20 @@ class Solution {
             }
         }
         
-        
         while(numMatches != 26 && right < s2.length()){
-            currentIndex = (int)(s2.charAt(right)) - 97;
-            s2Map[currentIndex]++;
+           
+            s2Map[(int)(s2.charAt(right)) - 97]++;
             
-            if(s2Map[currentIndex] == s1Map[currentIndex]){
-                numMatches++;
-            } else if (s2Map[currentIndex] - 1 == s1Map[currentIndex]){
+            if(s2Map[(int)(s2.charAt(right)) - 97] == (s1Map[(int)(s2.charAt(right)) - 97])){
+                numMatches++; 
+            } else if (s2Map[(int)(s2.charAt(right)) - 97] - 1== (s1Map[(int)(s2.charAt(right)) - 97])){
                 numMatches--;
             }
             
-            currentIndex = (int)(s2.charAt(left)) - 97;
-            
-            s2Map[currentIndex]--;
-            
-            if(s2Map[currentIndex] == s1Map[currentIndex]){
+            s2Map[(int)(s2.charAt(left)) - 97]--;
+            if(s2Map[(int)(s2.charAt(left)) - 97] == (s1Map[(int)(s2.charAt(left)) - 97])){
                 numMatches++;
-            } else if (s1Map[currentIndex] - 1 == s2Map[currentIndex]){
+            } else if (s2Map[(int)(s2.charAt(left)) - 97] == (s1Map[(int)(s2.charAt(left)) - 97]) - 1){
                 numMatches--;
             }
             
@@ -67,6 +43,10 @@ class Solution {
             right++;
         }
         
-        return numMatches == 26;
+        if(numMatches == 26){
+            return true;
+        }
+        
+        return false;
     }
 }
